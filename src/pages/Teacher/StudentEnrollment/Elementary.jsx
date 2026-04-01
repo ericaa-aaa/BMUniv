b377f7afb8146bd1ceed4453f9894a3784c5de8c
erica <javilloericajane@gmail.com>
2026-04-01 14:55:32 +0800

    port commit

--- src/pages/Teacher/StudentEnrollment/Elementary.jsx
+++ src/pages/Teacher/StudentEnrollment/Elementary.jsx
@@ -1,7 +1,15 @@
 import SideBar from '../MainDashboard/Sidebar';
+import Page1 from "../StudentEnrollment/ElementaryPages/Page1"
+import Page2 from "../StudentEnrollment/ElementaryPages/Page2"
+import { useState } from 'react'
 
 export default function Elementary() {
+
+    const [page, setPage] = useState(1);
+
     return (
+
+
         <div className='flex h-screen'>
   <div className='w-67'>
   <SideBar />
@@ -15,127 +23,28 @@ export default function Elementary() {
                     <p className="flex justify-center text-[#EDEBDD] font-['Inter'] bg-[#630000] w-130 font-semibold text-[30px] rounded-2xl p-3">Elementary Enrollment Form</p>
                 </div>
 
-                <div className="pt-9 pl-12 flex gap-5">
-                    <p className="text-[#630000] font-['Inter'] text-[25px] font-semibold">Grade Level:</p>
-                    <input type="text" className="border text-[12px] w-20 h-9 p-3 rounded-[5px] font-['Inter']" required />
-                </div>
+                {page === 1 && <Page1 />}
+                {page === 2 && <Page2 />}
 
-                <div className="pt-10 pl-12">
-                    <p className="text-[#630000] font-['Inter'] text-[25px] font-semibold">Student Information</p>
-                </div>
+                    <div className="flex justify-end mt-5 gap-2">
+                        <button 
+                            onClick={() => setPage(page - 1)} 
+                            disabled={page === 1}
+                            className="px-3 py-1 bg-gray-300"
+                            >
+                            {"<"}
+                        </button>
 
-                <div className="bg-[#EDEBDD] w-370 h-50 ml-12 rounded-2xl">
-                    <div className="flex gap-5">
-                        <p className="text-[#1B1717] text-[14px] pt-9 pl-12">Student Name:</p>
-                        <div className="pt-6.5 flex gap-5">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="First Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Middle Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-9 pl-7">Ext.</p>
-                        <div className="pt-6.5">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-9 pl-7">Age:</p>
-                        <div className="pt-6.5">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                    </div>
+                        <span className="px-3 py-1 bg-[#630000] text-white">{page}</span>
 
-                    <div className="flex gap-5">
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-12">Birth Date:</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
+                            <button 
+                                onClick={() => setPage(page + 1)} 
+                                disabled={page === 2}
+                                className="px-3 py-1 bg-[#630000] text-white">
+                                {">"}
+                            </button>
                         </div>
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-7">Place of Birth</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-7">Civil Status</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-7">Gender</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                    </div>
-
-                    <div className="flex gap-5">
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-12">Mother Tounge</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-7">Religion</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-7">Weight</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-7">Height</p>
-                        <div className="pt-4">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex gap-5 pl-16">
-                    <p className='text-[#1B1717] text-[14px] pt-5 pl-7'>Belonging to any Indigenous Peoples (IP) Community/Indigenous Cultural Community?</p>
-                    <div className='pt-5'>
-                        <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 mr-3 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft"></input>
-                        <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft"></input>
-                    </div>
-                </div>
-
-                <div className="pt-5 pl-12">
-                    <p className="text-[#630000] font-['Inter'] text-[25px] font-semibold">Contact Information</p>
-                </div>
-
-                <div className="bg-[#EDEBDD] w-370 h-36 ml-12 rounded-2xl">
-                    <div className="flex gap-5">
-                        <p className="text-[#1B1717] text-[14px] pt-9 pl-12">Contact Number</p>
-                        <div className="pt-6.5 flex gap-5">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                        <p className="text-[#1B1717] text-[14px] pt-9 pl-7">Email Address</p>
-                        <div className="pt-6.5">
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                        </div>
-                    </div>
-
-                    <div className="flex gap-5">
-                        <p className="text-[#1B1717] text-[14px] pt-7 pl-12">Current Address</p>
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-4 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-4 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-4 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-4 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-4 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                    </div>
-                </div>
-
-                <div className="flex gap-5 pl-16">
-                    <p className='text-[#1B1717] text-[14px] pt-5 pl-7'>Current address is the same as permanent address? </p>
-                    <div className='pt-5'>
-                        <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 mr-3 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft"></input>
-                        <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft"></input>
-                    </div>
-                </div>
-
-                <div className="bg-[#EDEBDD] w-370 h-22 ml-12 rounded-2xl mt-5">     
-
-                    <div className="flex gap-5">
-                        <p className="text-[#1B1717] text-[14px] pt-9 pl-12">Current Address</p>
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-7 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-7 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-7 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-7 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                            <input type="text" className="border text-[12px] w-50 h-10 p-3 mt-7 rounded-[5px] font-['Inter']" placeholder="Last Name" required />
-                    </div>
-                </div>
-
-            </div>
+             </div>
         </section>
  </div>
     )
