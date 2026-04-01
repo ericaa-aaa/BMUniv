211eeb3e258f2ee5be497590836777ce45e427a2
llyne <22-08549@g.batstate-u.edu.ph>
2026-04-01 10:35:17 +0800

    teacher dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './index.css';
 import LoginPage from "./pages/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TeacherDashboard from './pages/Teacher/TeacherDashboard';
 import StudentDashboard from './pages/Student/StudentDashboard';
+
+import Dashboard from './pages/Teacher/MainDashboard/Dashboard';
+import SideBar from './pages/Student/Sidebar';
 
 export default function App() {
   return (
@@ -10,7 +12,7 @@ export default function App() {
     <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/teacher/*" element={<TeacherDashboard/>} />
+          <Route path="/teacher/*" element={<Dashboard/>} />
           <Route path="/student/*" element={<StudentDashboard/>} />
         </Routes>
       </Router>
---
+++ src/pages/Teacher/MainDashboard/Dashboard.jsx
@@ -0,0 +1,55 @@
+import profile from '/home/dev/FrontendEnrollment/BMUniv/src/assets/images/faculty1.png'
+
+import SideBar from '/home/dev/FrontendEnrollment/BMUniv/src/pages/Teacher/MainDashboard/Sidebar.jsx';
+
+function Dashboard() {
+  return (
+<div className='flex h-screen'>
+  <div className='w-64'>
+  <SideBar />
+  </div>
+    <div className="flex-1 bg-[url('/home/dev/FrontendEnrollment/BMUniv/src/assets/images/bg.jpg')] bg-cover bg-no-repeat relative bg-position-[50%_25%]">
+        
+      <div className="absolute inset-0 bg-white/75"></div>
+
+        <div className="relative p-8">
+
+
+        
+
+        <div className="flex justify-end mr-10">
+          <p className='pt-12 font-["Inter"] text-[#1B1717] font-medium'>Teacher Juan Dela Cruz</p>
+          <img src={profile} alt='Faculty' className='w-30 h-30'></img>
+        </div>
+
+
+        <div className="flex justify-around mt-17 font-['Inter'] font-semibold ">
+          <div className="bg-[#630000] rounded-xl w-70 h-35 text-center pt-6">
+            <h2 className="text-2xl font-bold text-[#EDEBDD]">100</h2>
+            <div className='w-55 h-0.5 bg-white mt-5 mb-2 ml-7'></div>
+            <p className='text-[20px] text-[#EDEBDD]'>Elementary Students</p>
+          </div>
+
+          <div className="bg-[#630000] rounded-xl w-70 h-35 text-center pt-6">
+            <h2 className="text-2xl text-[#EDEBDD] font-bold">10</h2>
+            <div className='w-55 h-0.5 bg-white mt-5 mb-2 ml-7'></div>
+            <p className='text-[20px] text-[#EDEBDD]'>High School Enrolled</p>
+          </div>
+
+          <div className="bg-[#630000] rounded-xl w-70 h-35 text-center pt-6">
+            <h2 className="text-2xl font-bold text-[#EDEBDD]">100</h2>
+            <div className='w-55 h-0.5 bg-white mt-5 mb-2 ml-7'></div>
+            <p className='text-[20px] text-[#EDEBDD]'>SHS Enrolled</p>
+          </div>
+        </div>
+
+        <div className='bg-[#EDEBDD] w-340 h-132 flex ml-19 mt-11 rounded-[15px]'></div>
+
+      </div>
+    </div>
+    </div>
+     
+  );
+};
+
+export default Dashboard;
