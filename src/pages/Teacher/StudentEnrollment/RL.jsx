749eb338484f0a7fe0c5eec0f2df690705abcd4a
llyne <22-08549@g.batstate-u.edu.ph>
2026-04-08 13:09:38 +0800

    student enrollment

--- src/pages/Teacher/StudentEnrollment/RL.jsx
+++ src/pages/Teacher/StudentEnrollment/RL.jsx
@@ -1,7 +1,5 @@
 
 import Page1 from "../StudentEnrollment/ReturningPages/Page1"
-import Page2 from "../StudentEnrollment/ReturningPages/Page2"
-import LastPage from "../StudentEnrollment/ReturningPages/LastPage"
 import back from "../../../assets/images/bg.jpg"
 import { useState } from 'react'
 
@@ -11,7 +9,7 @@ export default function Elementary() {
 
     return (
 
-        <section className="h-screen bg-cover bg-no-repeat bg-fixed bg-center"
+        <section className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center"
             style={{ backgroundImage: `url(${back})` }}>
 
             <div className="min-h-screen bg-white/75 p-4">
@@ -20,28 +18,7 @@ export default function Elementary() {
                     <p className="flex justify-center text-[#EDEBDD] font-['Inter'] bg-[#630000] w-130 font-semibold text-[30px] rounded-2xl p-3">RL Enrollment Form</p>
                 </div>
 
-                {page === 1 && <Page1 />}
-                {page === 2 && <Page2 />}
-                {page === 3 && <LastPage />}
-
-                    <div className="flex justify-end mt-5 gap-2">
-                        <button 
-                            onClick={() => setPage(page - 1)} 
-                            disabled={page === 1}
-                            className="px-3 py-1 bg-gray-300"
-                            >
-                            {"<"}
-                        </button>
-
-                        <span className="px-3 py-1 bg-[#630000] text-white">{page}</span>
-
-                            <button 
-                                onClick={() => setPage(page + 1)} 
-                                disabled={page === 3}
-                                className="px-3 py-1 bg-[#630000] text-white">
-                                {">"}
-                            </button>
-                        </div>
+                <Page1 />
              </div>
         </section>
     )
