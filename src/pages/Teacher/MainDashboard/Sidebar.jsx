7c935cd8daf853b37e007c1f41b6967dbeaa060d
llyne <22-08549@g.batstate-u.edu.ph>
2026-04-08 13:17:42 +0800

    bmu

--- src/pages/Teacher/MainDashboard/Sidebar.jsx
+++ src/pages/Teacher/MainDashboard/Sidebar.jsx
@@ -21,7 +21,7 @@ function Sidebar() {
       
       <div className='flex gap-4 pt-15 mb-10'>
         <img img src={e} alt='students' className='w-15 h-11'></img>
-        <h1 className="text-[25px] font-['Inter'] font-bold pt-1">Dashboard</h1>
+        <h1 className="text-[25px] font-['Inter'] font-bold pt-1">BMU</h1>
       </div>
 
       <nav className="flex flex-col gap-6 font-['Inter'] font-medium">
