f0c11c5e16bef70d0870f56ec790507f91772bb5
axeelle <axeldelasalas09@gmail.com>
2026-04-14 17:35:05 +0800

    try

--- src/services/elementarystudentservice.js
+++ src/services/elementarystudentservice.js
@@ -1,6 +1,5 @@
 // ElementaryStudentService.js
-
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const ElementaryStudentService = {
     // 1. Fetch all students
