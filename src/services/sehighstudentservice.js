bfda6c3c3b38421c5d1182f3679b650f97196adc
axeelle <axeldelasalas09@gmail.com>
2026-04-14 17:41:43 +0800

    tryyy

--- src/services/sehighstudentservice.js
+++ src/services/sehighstudentservice.js
@@ -1,6 +1,5 @@
 // HighSchoolStudentService.js (Consider renaming to ElementaryStudentService.js if that's the intent)
-
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const SeHighSchoolStudentService = {
     // 1. Fetch all students
