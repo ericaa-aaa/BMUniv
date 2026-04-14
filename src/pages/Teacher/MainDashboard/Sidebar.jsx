785edfeb322bffe72861df78ced8ac59c1a9ae4f
erica <javilloericajane@gmail.com>
2026-04-14 09:46:01 +0800

    sidebar

--- src/pages/Teacher/MainDashboard/Sidebar.jsx
+++ src/pages/Teacher/MainDashboard/Sidebar.jsx
@@ -92,24 +92,17 @@ function Sidebar() {
         
         
        <div>
-        <div 
-          onClick={() => setOpenRecords(!openRecords)}
+        <div onClick={() => setOpenRecords(!openRecords)}
           className={`group flex items-center gap-3 cursor-pointer py-2 px-7 rounded transition-colors ${openRecords 
-          ? 'bg-[#EDEBDD]' 
-          : 'hover:bg-[#EDEBDD]'}`}>
+            ? 'bg-[#EDEBDD]' 
+            : 'hover:bg-[#EDEBDD]'}`}>
           <div className="flex items-center gap-5 cursor-pointer">
             <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              width="23" 
-              height="23" 
-              viewBox="0 0 24 24" 
-              fill="none" 
-              stroke={openRecords ? "#1B1717" : "#EDEBDD"} 
-              strokeWidth="2" 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              className="transition-[stroke] duration-300 group-hover:stroke-[#1B1717]"
-            >
+              xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" 
+              fill="none" stroke={openRecords ? "#1B1717" : "#EDEBDD"}  strokeWidth="2" 
+              strokeLinecap="round" strokeLinejoin="round" 
+              className="transition-[stroke] duration-300 group-hover:stroke-[#1B1717]">
+                
               <path d="M15.5 2H11a2 2 0 0 0-2 2v1" />
               <path d="M7 18a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h7.5L17 4.5" />
               <path d="M11 7h7a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2Z" />
@@ -124,35 +117,26 @@ function Sidebar() {
         </div>
 
          {openRecords && (
-           <div className='ml-6 mt-2 flex flex-col gap-2 text-sm'>
-               <div onClick={() => setActiveItem("elementary")} className={`flex items-center gap-3 cursor-pointer rounded transition`}>
-                 <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "elementary"
-                   ? "bg-[#1B1717]"
-                   : "hover:bg-[#EDEBDD]"}`}></span>
-                   <Link to="/elemrec">Elementary Records</Link>
-              </div>
-
-                <div onClick={() => setActiveItem("jhs")} className={`flex items-center gap-3 cursor-pointer rounded transition`}>
-                 <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "jhs"
-                   ? "bg-[#1B1717]"
-                   : "hover:bg-[#EDEBDD]"}`}></span>
-                   <Link to="/jhsrec">Junior High School Records</Link>
-              </div>
-
-              <div onClick={() => setActiveItem("jhs")} className={`flex items-center gap-3 cursor-pointer rounded transition`}>
-                 <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "jhs"
-                   ? "bg-[#1B1717]"
-                   : "hover:bg-[#EDEBDD]"}`}></span>
-                   <Link to="/shsrec">Senior High School Records</Link>
-              </div>
-
-              <div onClick={() => setActiveItem("jhs")} className={`flex items-center gap-3 cursor-pointer rounded transition`}>
-                 <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "jhs"
-                   ? "bg-[#1B1717]"
-                   : "hover:bg-[#EDEBDD]"}`}></span>
-                   <Link to="/rlrec">Return Learners Records</Link>
-              </div>
-           </div>
+          <div className='ml-6 mt-2 flex flex-col gap-2 text-sm'>
+            <div onClick={() => setActiveItem("elementary")} className="flex items-center gap-3 cursor-pointer rounded transition">
+              <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "elementary" ? "bg-[#EDEBDD]" : "bg-[#1B1717]"}`}></span>
+              <Link to="/elemrec" className={`cursor-pointer ${activeItem === "elementary" ? 'underline' : 'hover:underline'}`}>
+                Elementary Records
+              </Link>
+            </div>
+            <div onClick={() => setActiveItem("jhs")} className="flex items-center gap-3 cursor-pointer rounded transition">
+              <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "jhs" ? "bg-[#EDEBDD]" : "bg-[#1B1717]"}`}></span>
+              <Link to="/jhsrec" className={`cursor-pointer ${activeItem === "jhs" ? 'underline' : 'hover:underline'}`}>
+                Junior High School Records
+              </Link>
+            </div>
+            <div onClick={() => setActiveItem("shs")} className="flex items-center gap-3 cursor-pointer rounded transition">
+              <span className={`w-3 h-3 rounded-full border-2 ${activeItem === "shs" ? "bg-[#EDEBDD]" : "bg-[#1B1717]"}`}></span>
+              <Link to="/shsrec" className={`cursor-pointer ${activeItem === "shs" ? 'underline' : 'hover:underline'}`}>
+                Senior High School Records
+              </Link>
+            </div>
+          </div>
          )}
        </div>
 
