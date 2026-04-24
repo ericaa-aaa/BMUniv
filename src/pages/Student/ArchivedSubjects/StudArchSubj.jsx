bdf08c3532b41c49d4e8d01762357f0e7637af9f
erica <javilloericajane@gmail.com>
2026-04-24 15:04:48 +0800

    studdash

--- src/pages/Student/ArchivedSubjects/StudArchSubj.jsx
+++ src/pages/Student/ArchivedSubjects/StudArchSubj.jsx
@@ -45,9 +45,18 @@ export default function ArchivedSubjects() {
       >
         <div className="absolute inset-0 bg-white/70 flex items-center justify-between px-10">
           <div className="flex items-center gap-5">
-            <div className="w-24 h-24 bg-black rounded-full flex items-center justify-center">
-              <User size={50} className="text-white" />
-            </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-32"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             <h2 className="text-2xl font-semibold">Dela Cruz, Juan M.</h2>
           </div>
           <div className="pr-17 space-y-3 text-lg font-semibold text-[#7B0000]">
