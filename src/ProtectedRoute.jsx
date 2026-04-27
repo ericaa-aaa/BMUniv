8395ea693b340d78278a49cb47138f972ee23eb5
axeelle <axeldelasalas09@gmail.com>
2026-04-27 13:26:21 +0800

    pr with ver

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -4,10 +4,20 @@ import { useEffect, useState } from "react";
 export default function ProtectedRoute() {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    setIsAuthenticated(!!token);
-  }, []);
+useEffect(() => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    setIsAuthenticated(false);
+    return;
+  }
+
+  fetch(`${import.meta.env.VITE_BASE_URL}/verify-token`,
+          {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  .then(res => setIsAuthenticated(res.ok))
+  .catch(() => setIsAuthenticated(false));
+}, []);
 
   if (isAuthenticated === null) {
     return <div>Loading...</div>;
