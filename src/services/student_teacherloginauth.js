3a0df599f015dfc41af346985bdbb4cb6bcd01ae
axeelle <axeldelasalas09@gmail.com>
2026-04-27 11:56:52 +0800

    axel

--- src/services/student_teacherloginauth.js
+++ src/services/student_teacherloginauth.js
@@ -24,19 +24,6 @@ export const loginUser = async (username, password) => {
 
     const data = await res.json();
 
-    // if (res.ok) {
-    //   localStorage.setItem("token", data.access_token);
-    //   localStorage.setItem("role", data.role);
-    //   localStorage.setItem("activeUser", data.username);
-      
-    //   if (data.role === 'student') {
-    //     localStorage.setItem("studentGrade", data.grade || "");
-    //     localStorage.setItem("studentSection", data.section || "");
-    //     localStorage.setItem("studentCategory", data.category || "");
-    //     localStorage.setItem("studentStatus", data.status || "ENROLLED");
-    //   }
-    //   return { success: true, role: data.role };
-    // }
 
     if (res.ok) {
   localStorage.setItem("token", data.access_token);
