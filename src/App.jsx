1ceb91928bcbf1deed3a4c33262cad1d7722f983
axeelle <axeldelasalas09@gmail.com>
2026-04-29 17:19:25 +0800

    improved pr

--- src/App.jsx
+++ src/App.jsx
@@ -43,14 +43,14 @@ function AppContent() {
 
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route element={<ProtectedRoute />}> 
+        <Route element={<ProtectedRoute allowedRoles={['student']} />}>
         <Route path="/student" element={<StudentLayout />}>
           <Route index element={<StudentDashboard />} />
           <Route path="/student/studsubj/*" element={<StudSubj />} />
           <Route path="/student/archsubj/*" element={<ArchSubj />} />
         </Route>
         </Route>
-     <Route element={<ProtectedRoute />}> 
+      <Route element={<ProtectedRoute allowedRoles={['teacher']} />}>
         <Route path="/teacher" element={<TeacherLayout />}>
           <Route index element={<TeacherDashboard />} />
           <Route path="/teacher/elem/*" element={<ElementaryEnrollment />} />
