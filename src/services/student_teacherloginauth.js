52dd2769aa932c588561b4cbe33e72d248299662
axeelle <axeldelasalas09@gmail.com>
2026-04-30 15:51:52 +0800

    fixteachername

--- src/services/student_teacherloginauth.js
+++ src/services/student_teacherloginauth.js
@@ -37,8 +37,8 @@ export const loginUser = async (username, password) => {
   }
     else if (data.primary_role === 'teacher') {
     localStorage.setItem("teacherGrade", data.grade || "N/A");
-    localStorage.setItem("teacherFullName", data.teacher_fullname)
-    localStorage.setItem("teacherEmail", data.email)
+    localStorage.setItem("teacherFullName", data.teacherfullname)
+    localStorage.setItem("teacherEmail", data.email)  
     localStorage.setItem("teacherTitle", data.position)
   }
 
