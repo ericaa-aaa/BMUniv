bd245f84a00c718ef442024819b4ca120faaa276
llyne <liyensey@gmail.com>
2026-04-30 16:31:21 +0800

    required photo hs

--- src/pages/Teacher/StudentEnrollment/HighSchoolPages/Page1.jsx
+++ src/pages/Teacher/StudentEnrollment/HighSchoolPages/Page1.jsx
@@ -97,6 +97,7 @@ export default function EnrollmentForm() {
 
     if (step === 1) {
       fieldsToValidate = [
+        "photo",
         "lastname",
         "firstname",
         "age",
@@ -178,7 +179,7 @@ export default function EnrollmentForm() {
             success: <b>Enrollment submitted successfully!</b>,
             error: (err) => (
               <b>
-                {err.message === "SESSION_EXPIRED"
+                {err.message === SESSION_EXPIRED
                   ? "Session Expired"
                   : "Submission Failed"}
               </b>
@@ -193,12 +194,16 @@ export default function EnrollmentForm() {
         setStep((prev) => prev + 1);
       }
     } else {
-      let errorMessage = "PLease fill all required fields.";
-
+      // Determine the most specific error message
+      let errorMessage = "Please fill all required fields.";
       if (errors.grade_level) {
         errorMessage =
-          "Please enter a valid grade (7-10) and fill all the required fields.";
+          "Please enter a valid grade (1-6) and fill all required fields.";
+      } else if (errors.photo) {
+        errorMessage = "Please put an image";
       }
+
+      // Show only one error toast
       toast.error(errorMessage, {
         position: "top-right",
         style: { borderRadius: "10px", background: "#333", color: "#fff" },
@@ -267,7 +272,12 @@ export default function EnrollmentForm() {
             type="file"
             id="p-input"
             className="hidden"
-            {...register("photo")}
+            {...register("photo", {
+              validate: (value) => {
+                if (!value || value.length === 0) return "Photo is required";
+                return true;
+              },
+            })}
             accept="image/*"
           />
 
