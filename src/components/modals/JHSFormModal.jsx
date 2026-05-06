1a4054abdb6c13ce892d0afee661d349ede82498
erica <javilloericajane@gmail.com>
2026-05-06 11:05:12 +0800

    modal (x)

--- src/components/modals/JHSFormModal.jsx
+++ src/components/modals/JHSFormModal.jsx
@@ -102,7 +102,9 @@ export default function JHSFormModal({
               onClick={() => setShowModal(false)}
               className="absolute top-6 right-6 text-gray-400 hover:text-red-800 transition-colors bg-gray-50 p-1 rounded-full disabled:opacity-50"
             >
-              <IoMdCloseCircleOutline size={28} />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
             </button>
 
             <div className="bg-[#630000] font-['Inter'] text-white text-center py-5 rounded-2xl mb-10 shadow-md">
